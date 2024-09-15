eeaf43709528797e03a462a2077f315705d7edeb
ЧЕЛОВЕК ЧЕЛОВЕК <olegoviz.2006@gmail.com>
2024-09-15 14:47:09 +0500

    Task0

--- Tyuiu.RubanovEO.Sprint1.Task0.V21.Test/DataServiceTest.cs
+++ Tyuiu.RubanovEO.Sprint1.Task0.V21.Test/DataServiceTest.cs
@@ -1,14 +1,18 @@
-﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
-using System;
+
 using Tyuiu.RubanovEO.Sprint1.Task0.V21.Lib;
+
 
 namespace Tyuiu.RubanovEO.Sprint1.Task0.V21.Test
 {
-    [TestClass]
     public class DataServiceTest
     {
-        [TestMethod]
-        public void ValidExpression()
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
         {
             DataService ds = new DataService();
             var res = ds.Calculate();
--- Tyuiu.RubanovEO.Sprint1.Task0.V21/Program.cs
+++ Tyuiu.RubanovEO.Sprint1.Task0.V21/Program.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using Tyuiu.RubanovEO.Sprint1.Task0.V21.Lib;
+﻿using Tyuiu.RubanovEO.Sprint1.Task0.V21.Lib;
 
 namespace Tyuiu.RubanovEO.Sprint1.Task0.V21
 {
@@ -13,32 +8,29 @@ namespace Tyuiu.RubanovEO.Sprint1.Task0.V21
         {
             DataService ds = new DataService();
 
-
-            Console.Title = "Спринт #1 | Выполнил: Рубанов Е. О. | ПКТБ-24-1";
+            Console.Title = "Спринт #1 | Выполнил: Рубанов Е. О. | ПКТБ-24-1"; 
+            Console.WriteLine("***************************************************************************"); 
+            Console.WriteLine("* Спринт #1                                                               *"); 
+            Console.WriteLine("* Тема: Базовые навыки работы в C#                                        *"); 
+            Console.WriteLine("* Задание #0                                                              *"); 
+            Console.WriteLine("* Вариант #21                                                             *"); 
+            Console.WriteLine("* Выполнил: Рубанов Егор Олегович | ПКТБ-24-1                             *"); 
+            Console.WriteLine("***************************************************************************"); 
+            Console.WriteLine("* УСЛОВИЕ:                                                                *"); 
+            Console.WriteLine("* Написать программу, которая вычисляет выражение 10 / (2 + 3)            *"); 
+            Console.WriteLine("* и печатает результат на экране                                          *"); 
+            Console.WriteLine("*                                                                         *"); 
+            Console.WriteLine("***************************************************************************"); 
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *"); 
+            Console.WriteLine("***************************************************************************"); 
+            Console.WriteLine("* 10 / (2 + 3)                                                            *"); 
+            Console.WriteLine("***************************************************************************"); 
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *"); 
             Console.WriteLine("***************************************************************************");
-            Console.WriteLine("* Спринт #1                                                               *");
-            Console.WriteLine("* Тема: Базовые навыки работы в C#                                        *");
-            Console.WriteLine("* Задание #0                                                              *");
-            Console.WriteLine("* Вариант #21                                                             *");
-            Console.WriteLine("* Выполнил: Рубанов Егор Олегович | ПКТБ-24-1                             *");
-            Console.WriteLine("***************************************************************************");
-            Console.WriteLine("* УСЛОВИЕ:                                                                *");
-            Console.WriteLine("* Написать программу, которая вычисляет выражение 10 / (2 + 3)            *");
-            Console.WriteLine("* и печатает результат на экране                                          *");
-            Console.WriteLine("*                                                                         *");
-            Console.WriteLine("***************************************************************************");
-            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
-            Console.WriteLine("***************************************************************************");
-            Console.WriteLine("* 10 / (2 + 3)                                                            *");
-            Console.WriteLine("***************************************************************************");
-            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
-            Console.WriteLine("***************************************************************************");
-
 
             Console.WriteLine(ds.Calculate());
 
             Console.ReadKey();
-
         }
     }
 }
